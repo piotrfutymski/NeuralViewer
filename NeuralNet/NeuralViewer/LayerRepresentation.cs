b50c6a8fc51c5741cbbd6493c6bec600593a36cb
Piotr Futymski <piotr.futymski@wp.pl>
2018-09-16 15:36:01 +0200

    Added neuron representation class

--- NeuralNet/NeuralViewer/LayerRepresentation.cs
+++ NeuralNet/NeuralViewer/LayerRepresentation.cs
@@ -15,6 +15,8 @@ namespace NeuralViewer
         protected double baseWidthOnScreen;
         protected Canvas layerScreen;
 
+
+
         public LayerRepresentation(Canvas screen)
         {
             baseHeightOnScreen = screen.Height;
---
+++ NeuralNet/NeuralViewer/NeuronRepresentation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralViewer
+{
+    class NeuronRepresentation
+    {
+        public double Value { get; private set; }
+
+        public NeuronRepresentation()
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+            Value = r.NextDouble();
+        }
+    }
+}
