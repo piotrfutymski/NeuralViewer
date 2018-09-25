a713650c868abe1f32baa5b3555a80c6306f9bb3
Piotr Futymski <piotr.futymski@wp.pl>
2018-09-25 19:04:49 +0200

    small changes

--- NeuralNet/NeuralNet/Network.cs
+++ NeuralNet/NeuralNet/Network.cs
@@ -81,7 +81,6 @@ namespace NeuralNet
             return new NetworkState(mConections, s);
         }
 
-
         public int Train(int s, List<Sample> sampList, double DELTA = 0.1, int numToSum = 25)
         {
             int how_many_times_upgraded = 0;
@@ -126,6 +125,50 @@ namespace NeuralNet
                 }
             }
             return gradientList;
+        }
+
+
+        public int TrainAsync(int s, List<Sample> sampList, double DELTA = 0.1, int numToSum = 25)
+        {
+            int how_many_times_upgraded = 0;
+
+            var maxTime = new TimeSpan(0, 0, s);
+            var actTime = new TimeSpan(0);
+            int num = sampList.Count;
+
+            var tasks = GetBeginingTasks(sampList, numToSum);
+
+            while (actTime < maxTime)
+            {
+                var startTime = DateTime.Now;
+                
+                Task.Factory.ContinueWhenAny(tasks, (winner) =>
+                {
+                    var gradientList = winner.Result;
+                    for (int i = 0; i < mConections.Count; i++)
+                    {
+                        gradientList[i] = gradientList[i] * DELTA;
+                        mConections[i] += gradientList[i];
+                    }
+
+                    winner.Dispose();
+                    if (actTime < maxTime)
+                        winner = Task<List<ConectionsInfo>>.Factory.StartNew(() => { return CountMinusGradient(sampList, numToSum, new NetworkState(mConections, sampList[0])); });
+
+                    how_many_times_upgraded += numToSum;
+                });
+                Task.Factory.ContinueWhenAll(tasks, (a) =>
+                {
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        a[i].Dispose();
+                    } }).Wait();
+
+                actTime += DateTime.Now - startTime;
+            }
+
+            Save();
+            return how_many_times_upgraded;
         }
 
         Task<List<ConectionsInfo>>[] GetBeginingTasks(List<Sample> sampList, int numToSum)
